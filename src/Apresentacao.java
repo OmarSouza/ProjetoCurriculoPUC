708d6d045d2e7519440abd677e36f9e59b581c63
OmarSouza <73590497+OmarSouza@users.noreply.github.com>
2021-11-08 09:12:33 -0300

    correçao

--- src/Apresentacao.java
+++ src/Apresentacao.java
@@ -1,15 +1,5 @@
-
 import java.util.concurrent.TimeUnit;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Rebekah
- */
 public class Apresentacao {
     public void inicio(){
         System.out.println("Bem Vindo (a)");
@@ -21,5 +11,9 @@ public class Apresentacao {
         System.out.println("Aguarde...");
         TimeUnit.SECONDS.sleep(10);
         System.out.println("Curriculo Emitido com Sucesso");
+    }
+    
+    public void finalizacao(){
+        System.out.println("Obrigado por utilizar o programa.");
     }
 }
