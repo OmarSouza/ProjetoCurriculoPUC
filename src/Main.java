bfc71d70f7103d8200ff5dc185a4353520301664
OmarSouza <73590497+OmarSouza@users.noreply.github.com>
2021-11-22 21:59:28 -0300

    Commit Semifinafinal

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        Apresentacao presentation = Apresentacao.getInstance();
-        Scanner scan = new Scanner(System.in);
-        Pessoa pessoa = Pessoa.getInstance();
-        
-        presentation.inicio();
-        
-        System.out.println("Informe seu nome");
-        pessoa.setNome_Pessoa(scan.nextLine());
-        System.out.println(pessoa.getNome_Pessoa());
+        MontagemCurriculo montCur = new MontagemCurriculo();
+        montCur.inicioPrograma();
                 
     }
     
